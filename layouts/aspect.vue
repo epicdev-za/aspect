d54b0ce5007ba53861e1481eb2928e5bf4c312a0
Orion <admin@ot-sdt-serv.com>
2021-01-23 07:40:46 +0200

    WIP

--- layouts/aspect.vue
+++ layouts/aspect.vue
@@ -64,9 +64,124 @@
                                             </v-col>
                                         </v-row>
                                     </v-container>
+                                    <v-list>
+                                        <v-list-item @click="">
+                                            <v-list-item-content>
+                                                <v-list-item-title>Facebook SEO Settings</v-list-item-title>
+                                            </v-list-item-content>
+                                            <v-list-item-icon>
+                                                <v-icon>mdi-chevron-right</v-icon>
+                                            </v-list-item-icon>
+                                        </v-list-item>
+                                        <v-list-item @click="">
+                                            <v-list-item-content>
+                                                <v-list-item-title>Twitter SEO Settings</v-list-item-title>
+                                            </v-list-item-content>
+                                            <v-list-item-icon>
+                                                <v-icon>mdi-chevron-right</v-icon>
+                                            </v-list-item-icon>
+                                        </v-list-item>
+                                        <v-list-item @click="">
+                                            <v-list-item-content>
+                                                <v-list-item-title>Instagram SEO Settings</v-list-item-title>
+                                            </v-list-item-content>
+                                            <v-list-item-icon>
+                                                <v-icon>mdi-chevron-right</v-icon>
+                                            </v-list-item-icon>
+                                        </v-list-item>
+                                    </v-list>
                                 </v-sheet>
-                                <v-sheet v-if="aspect.menu.sub_tab === 1" class="pa-4">
-
+                                <v-sheet v-if="aspect.menu.sub_tab === 1">
+                                    <v-list>
+                                        <v-list-item>
+                                            <v-list-item-icon>
+                                                <v-icon>mdi-chevron-right</v-icon>
+                                            </v-list-item-icon>
+                                            <v-list-item-content>
+                                                <v-list-item-title>Banner</v-list-item-title>
+                                            </v-list-item-content>
+                                            <v-list-item-action>
+                                                <v-btn depressed>Add</v-btn>
+                                            </v-list-item-action>
+                                        </v-list-item>
+                                        <v-list-item>
+                                            <v-list-item-icon>
+                                                <v-icon>mdi-image-text</v-icon>
+                                            </v-list-item-icon>
+                                            <v-list-item-content>
+                                                <v-list-item-title>Text with Image</v-list-item-title>
+                                            </v-list-item-content>
+                                            <v-list-item-action>
+                                                <v-btn depressed>Add</v-btn>
+                                            </v-list-item-action>
+                                        </v-list-item>
+                                        <v-list-item>
+                                            <v-list-item-icon>
+                                                <v-icon>mdi-text</v-icon>
+                                            </v-list-item-icon>
+                                            <v-list-item-content>
+                                                <v-list-item-title>Text</v-list-item-title>
+                                            </v-list-item-content>
+                                            <v-list-item-action>
+                                                <v-btn depressed>Add</v-btn>
+                                            </v-list-item-action>
+                                        </v-list-item>
+                                        <v-list-item>
+                                            <v-list-item-icon>
+                                                <v-icon>mdi-chevron-right</v-icon>
+                                            </v-list-item-icon>
+                                            <v-list-item-content>
+                                                <v-list-item-title>Image and Text Grid</v-list-item-title>
+                                            </v-list-item-content>
+                                            <v-list-item-action>
+                                                <v-btn depressed>Add</v-btn>
+                                            </v-list-item-action>
+                                        </v-list-item>
+                                        <v-list-item>
+                                            <v-list-item-icon>
+                                                <v-icon>mdi-apps</v-icon>
+                                            </v-list-item-icon>
+                                            <v-list-item-content>
+                                                <v-list-item-title>Image Grid</v-list-item-title>
+                                            </v-list-item-content>
+                                            <v-list-item-action>
+                                                <v-btn depressed>Add</v-btn>
+                                            </v-list-item-action>
+                                        </v-list-item>
+                                        <v-list-item>
+                                            <v-list-item-icon>
+                                                <v-icon>mdi-chevron-right</v-icon>
+                                            </v-list-item-icon>
+                                            <v-list-item-content>
+                                                <v-list-item-title>Image Slider</v-list-item-title>
+                                            </v-list-item-content>
+                                            <v-list-item-action>
+                                                <v-btn depressed>Add</v-btn>
+                                            </v-list-item-action>
+                                        </v-list-item>
+                                        <v-list-item>
+                                            <v-list-item-icon>
+                                                <v-icon>mdi-chevron-right</v-icon>
+                                            </v-list-item-icon>
+                                            <v-list-item-content>
+                                                <v-list-item-title>Text Slider</v-list-item-title>
+                                            </v-list-item-content>
+                                            <v-list-item-action>
+                                                <v-btn depressed>Add</v-btn>
+                                            </v-list-item-action>
+                                        </v-list-item>
+                                        <v-list-item>
+                                            <v-list-item-icon>
+                                                <v-icon>mdi-image</v-icon>
+                                            </v-list-item-icon>
+                                            <v-list-item-content>
+                                                <v-list-item-title>Full Image</v-list-item-title>
+                                            </v-list-item-content>
+                                            <v-list-item-action>
+                                                <v-btn depressed>Add</v-btn>
+                                            </v-list-item-action>
+                                        </v-list-item>
+                                    </v-list>
                                 </v-sheet>
                             </v-tab-item>
                         </v-tabs-items>
